c46428554b0654b24ad1eec376f572fc56cf6f48
Fraser Dillon <fraserdillon050388@gmail.com>
2021-12-16 15:27:15 +0000

    Periodic commit

--- controllers/cities_controller.py
+++ controllers/cities_controller.py
@@ -11,18 +11,18 @@ cities_blueprint = Blueprint("cities", __name__)
 @cities_blueprint.route("/cities")
 def cities():
     cities = city_repository.select_all()
-    return render_template("cities/index.html", all_cities=cities)
+    return render_template("cities/index.html", cities = cities)
 
 @cities_blueprint.route("/cities/new", methods=["GET"])
 def new_city():
     cities = city_repository.select_all()
-    return render_template("cities/new.html", all_cities=cities)
+    return render_template("cities/new.html", cities = cities)
 
 @cities_blueprint.route("/cities", methods=["POST"])
 def create_city():
-    name = request.form["name"]
-    country = request.form["country"]
-    visited = request.form["visited"]
+    name = request.form['name']
+    country = request.form['country']
+    visited = request.form['visited']
     city = City(name, country, visited)
     city_repository.save(city)
     return redirect('/cities')
@@ -30,20 +30,20 @@ def create_city():
 @cities_blueprint.route("/cities/<id>", methods=["GET"])
 def show_city(id):
     city = city_repository.select(id)
-    return render_template("cities/show.html", city=city)
+    return render_template("cities/show.html", city = city)
 
 @cities_blueprint.route("/cities/<id>/edit", methods=["GET"])
 def edit_city(id):
     city  = city_repository.select(id)
     pubs = pub_repository.select_all()
-    return render_template("cities/edit.html", city=city, all_pubs=pubs)
+    return render_template("cities/edit.html", city = city, pubs = pubs)
 
 @cities_blueprint.route("/cities/<id>", methods=["POST"])
 def update_city(id):
-    name = request.form["name"]
-    country = request.form["country"]
-    visited = request.form["visited"]
-    city = City(name, country, visited)
+    name = request.form['name']
+    country = request.form['country']
+    visited = request.form['visited']
+    city = City(name, country, visited, id)
     city_repository.update(city)
     return redirect('/cities')
 
--- controllers/pubs_controller.py
+++ controllers/pubs_controller.py
@@ -11,18 +11,18 @@ pubs_blueprint = Blueprint("pubs", __name__)
 @pubs_blueprint.route("/pubs")
 def pubs():
     pubs = pub_repository.select_all()
-    return render_template("pubs/index.html", all_pubs=pubs)
+    return render_template("pubs/index.html", pubs = pubs)
 
 @pubs_blueprint.route("/pubs/new", methods=['GET'])
 def new_pub():
     cities = city_repository.select_all()
-    return render_template("cities/new.html", all_cities=cities)
+    return render_template("cities/new.html", cities = cities)
 
 @pubs_blueprint.route("/pubs", methods=['POST'])
 def create_pub():
-    name = request.form["name"]
-    city = city_repository.select(request.form["city_id"])
-    visited = request.form["visited"]
+    name = request.form['name']
+    city = city_repository.select(request.form['city_id'])
+    visited = request.form['visited']
     pub = Pub(name, city, id, visited)
     pub_repository.save(pub)
     return redirect('/pubs')
@@ -30,20 +30,20 @@ def create_pub():
 @pubs_blueprint.route("/pubs/<id>", methods=['GET'])
 def show_pub(id):
     pub = pub_repository.select(id)
-    return render_template("pubs/show.html", pub = pub, all_pubs=pubs)
+    return render_template("pubs/show.html", pub = pub, pubs = pubs)
 
 @pubs_blueprint.route("/pubs/<id>/edit", methods=['GET'])
 def edit_pubs(id):
     pub = pub_repository.select(id)
     cities = city_repository.select_all()
-    return render_template("pubs/edit.html", pub = pub, all_cities=cities)
+    return render_template("pubs/edit.html", pub = pub, cities = cities)
 
 @pubs_blueprint.route("/pubs/<id>", methods=['POST'])
 def update_pub(id):
-    name = request.form["name"]
-    city = city_repository.select(request.form["city_id"])
-    visited = request.form["visited"]
-    pub = Pub(name, city, id, visited)
+    name = request.form['name']
+    city = city_repository.select(request.form['city_id'])
+    visited = request.form['visited']
+    pub = Pub(name, city, visited, id)
     pub_repository.update(pub)
     return redirect('/pubs')
 
